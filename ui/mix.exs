5ee4222ccb98f42d945ef94000b035fd3ac51a7a
ChristianTovar <tovarchristian21@gmail.com>
2024-07-17 12:07:53 -0500

    Add ex_doc dependency

--- ui/mix.exs
+++ ui/mix.exs
@@ -51,7 +51,8 @@ defmodule Ui.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 
