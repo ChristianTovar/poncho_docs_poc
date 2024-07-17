1bb410c85f176c529e3803b340fdc7d98e11eb0d
ChristianTovar <tovarchristian21@gmail.com>
2024-07-17 11:53:08 -0500

    Initial poncho commit

---
+++ ui/config/test.exs
@@ -0,0 +1,18 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :ui, UiWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "OCLZS4JR65FQQeXidiShKYWWL0F1KTvCJ2Vh7aUhE39L8R+rkzgwDyw/tb/F00fg",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
