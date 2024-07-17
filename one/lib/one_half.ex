e08d82377c7f2838fb6b1ad3c8be1d442088e81a
ChristianTovar <tovarchristian21@gmail.com>
2024-07-17 14:43:31 -0500

    Add one half module

---
+++ one/lib/one_half.ex
@@ -0,0 +1,18 @@
+defmodule OneHalf do
+  @moduledoc """
+  Documentation for `One Half`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> One.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
