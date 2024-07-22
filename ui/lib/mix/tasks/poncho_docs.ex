911aacc0b833e179a7c21fee7b080e5e8d5c996e
ChristianTovar <tovarchristian21@gmail.com>
2024-07-22 12:12:36 -0500

    Create temp directory on mix task

--- ui/lib/mix/tasks/poncho_docs.ex
+++ ui/lib/mix/tasks/poncho_docs.ex
@@ -10,14 +10,17 @@ defmodule Mix.Tasks.PonchoDocs do
   def run(_args) do
     Mix.Shell.IO.info("Generating new poncho docs...")
 
+    # Create a temporary directory
+    Mix.Shell.cmd("mkdir lib/temp", fn _x -> nil end)
+
     # Iterate paths and copy files inside those paths
     Enum.each(@paths, &Mix.Shell.cmd("cp #{&1} ./lib/temp/", fn _x -> nil end))
 
     # Generate new docs
     Mix.Shell.cmd("mix docs", fn _x -> nil end)
 
-    # Purge copied files
-    Mix.Shell.cmd("rm ./lib/temp/*", fn _x -> nil end)
+    # Delete copied files
+    Mix.Shell.cmd("rm -rf lib/temp", fn _x -> nil end)
 
     Mix.Shell.IO.info("Finished!")
   end
